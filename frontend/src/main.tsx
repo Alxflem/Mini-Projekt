72aadbba42b32cda3ab2159fc42129fa7dc8f4c6
Oliver Bengtsson <58479844+LongeMan@users.noreply.github.com>
2024-04-17 14:12:59 +0200

    Login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import HomePage from "./pages/HomePage.tsx";
+
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+
 import { createBrowserRouter } from "react-router-dom";
 
 import { RouterProvider } from "react-router-dom";
@@ -10,7 +14,7 @@ import { RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <LoginPage />,
   },
 ]);
 
