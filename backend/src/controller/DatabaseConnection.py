6621b653f716f7c169c6e33e8b986e987d3767dc
Olof00 <ojidbratt@gmail.com>
2024-05-17 17:26:47 +0200

    Connection to database through singletone class

---
+++ backend/src/controller/DatabaseConnection.py
@@ -0,0 +1,38 @@
+import psycopg2
+from psycopg2 import pool
+
+class Database:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super(Database, cls).__new__(cls)
+            cls.__instance._initialized = False
+        return cls.__instance
+
+    def __init__(self):
+        if not self._initialized:
+            self.connection_pool = psycopg2.pool.SimpleConnectionPool(
+                1, 20,  # Minimum 1 connection, maximum 20 connections
+                user="an4119",
+                password="gh796kh6",
+                host="pgserver.mau.se",
+                port="5432",
+                database="g16_mini_project"
+            )
+            self._initialized = True  # Mark this instance as initialized
+
+    @staticmethod
+    def get_instance():
+        if Database.__instance is None:
+            Database()
+        return Database.__instance
+
+    def get_connection(self):
+        return self.connection_pool.getconn()
+
+    def return_connection(self, connection):
+        self.connection_pool.putconn(connection)
+
+    def close_all_connections(self):
+        self.connection_pool.closeall()
---
+++ backend/src/controller/test1.py
@@ -0,0 +1,19 @@
+from DatabaseConnection import Database
+
+try:
+    db_instance = Database.get_instance()
+    connection = db_instance.get_connection()
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM user_account")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+
+    db_instance.return_connection(connection)
+except Exception as e:
+    print(f"Failed to retrieve data: {e}")
+finally:
+    if 'db_instance' in locals() and db_instance:
+        db_instance.close_all_connections()
