47cf1201009fc3d51a85b43f28e3a78cc8a1a6ec
Ludvig Larsson <ludvig@senvestro.se>
2024-05-19 14:07:32 +0200

    Added file for Login
    This checks if the entered credentials of a user login exists in the database

---
+++ backend/src/controller/Login.py
@@ -0,0 +1,26 @@
+from DatabaseConnection import Database
+
+def login_user(username, password):
+
+    try:
+        db_instance = Database.get_instance()
+        connection = db_instance.get_connection()
+        cursor = connection.cursor()
+
+        query = "SELECT EXISTS (SELECT 1 FROM user_account WHERE username = %s AND password = %s)"
+        cursor.execute(query, (username, password))
+        exists = cursor.fetchone()[0]
+
+        if exists:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"Failed to retrieve data (Login): {e}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            db_instance.return_connection(connection)
+        if db_instance:
+            db_instance.close_all_connections()
