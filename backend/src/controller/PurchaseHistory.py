aa72c0a6c7d8ad56eca9e0f927cf2bc5226b8fef
Olof00 <ojidbratt@gmail.com>
2024-05-20 23:49:40 +0200

    Added function to add product to purchase history

--- backend/src/controller/PurchaseHistory.py
+++ backend/src/controller/PurchaseHistory.py
@@ -1,19 +1,13 @@
 from DatabaseConnection import Database
 
-def add_product(name, p_type, price, image_url, production_date, color, condition, seller):
+def add_product_to_history(user_id, product_id):
 
     try:
         db_instance = Database.get_instance()
         connection = db_instance.get_connection()
         cursor = connection.cursor()
 
-
-        cursor.execute("SELECT u_id FROM user_account WHERE email = %s)", (seller))
-        result = cursor.fetchone()
-        u_id  = result[0]
-
-        cursor.execute("INSERT INTO product (name, type, price, image, production_date, color, condition, available, seller) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                       (name, p_type, price, image_url, production_date, color, condition, True, u_id))
+        cursor.execute("INSERT INTO purchase_history(user_id, product_id) VALUES (%s, %s)", (user_id, product_id))
 
         connection.commit()
         cursor.close()
