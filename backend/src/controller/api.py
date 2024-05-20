52d86d8a9edefa6bd7a1ce64b57e1e57d7c7d54a
Olof00 <ojidbratt@gmail.com>
2024-05-20 15:13:11 +0200

    API call to get types of products

--- backend/src/controller/api.py
+++ backend/src/controller/api.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from DatabaseConnection import Database
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:5176"}})
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
 # Initialize the database instance
 db_instance = Database.get_instance()
@@ -46,9 +46,42 @@ def get_products():
         if connection:
             db_instance.return_connection(connection)
 
-@app.route('/api/data', methods=['GET'])
-def receive_data():
+def get_types():
+    connection = None
+    cursor = None
+    try:
+        connection = db_instance.get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT type_name FROM product_type")
+        rows = cursor.fetchall()
+
+        result = [
+            {
+                'type_name': row[0]
+            }
+            for row in rows
+        ]
+
+        return jsonify(result), 200
+
+    except Exception as e:
+        print(f"Failed to retrieve data: {e}")
+        return jsonify({'error': 'Failed to retrieve data'}), 500
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            db_instance.return_connection(connection)
+
+@app.route('/api/products', methods=['GET'])
+def receive_products():
     return get_products()
+
+@app.route('/api/types', methods=['GET'])
+def receive_types():
+    return get_types()
 
 if __name__ == '__main__':
     app.run(debug=True)
