2655d8f88e2d859184fffdaff6f0182a86d92357
Mihail Spasovski <124456259+suizmuiz@users.noreply.github.com>
2024-05-21 07:49:18 +0200

    not neeeded elements in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,10 @@ import LoginPage from "./pages/LoginPage";
 import TestPage from "./pages/TestPage";
 import RegisterPage from "./pages/RegisterPage";
 import SellPage from "./pages/SellPage";
-
 import ProfilePage from "./pages/HomePage";
-import Header, { CartItem } from "./components/Header";
-import { useState } from "react";
 import { UserProvider } from "./components/UserContext";
 
 const App = () => {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   return (
     <div>
@@ -24,9 +20,7 @@ const App = () => {
             <Route path="/" element={<LoginPage/>} />
             <Route path="/landing" element={<LandingPage />} />
             <Route path="/reg" element={<RegisterPage />} />
-
             <Route path="/profile" element={<ProfilePage />} />
-
             <Route path="/products" element={<ProductsPage />} />
             <Route path="/test" element={<TestPage />} />
             <Route path="/sell" element={<SellPage />} />
